9efaf1de505caa38402890a8a4d4173f2dca332f
vrmSTEM <90628426+vrmSTEM@users.noreply.github.com>
2021-09-13 10:08:13 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    bird.change(LedSpriteProperty.Y, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    bird.change(LedSpriteProperty.Y, 1)
+})
+let bird: game.LedSprite = null
+bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
